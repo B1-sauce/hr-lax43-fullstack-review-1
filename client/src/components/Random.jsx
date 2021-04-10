4e8631c56b620a4f2a000124d5d1afe2f97552ca
jkhz <jhertz1@gmail.com>
2021-04-09 23:00:26 -0600

    Add student solution

--- client/src/components/Random.jsx
+++ client/src/components/Random.jsx
@@ -5,18 +5,27 @@ import React from 'react';
   There are multiple ways to implement this feature.
   How would you implement this as a functional component?
 */
+
 export default class Random extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-
+      randomStudent: {}
     }
+    this.getRandomStudent = this.getRandomStudent.bind(this);
+  }
+
+  componentDidMount() {
+    this.getRandomStudent()
   }
 
   getRandomStudent(){
     // this gives us a random index value
     var ind = Math.floor(Math.random() * this.props.students.length);
     // Todo: Add your logic here to grab one random student
+    this.setState({
+      randomStudent: this.props.students[ind]
+    })
 
   }
 
@@ -24,10 +33,10 @@ export default class Random extends React.Component {
     return (
       <div>
         <div>
-          <img src='https://ca.slack-edge.com/TMCQR98LA-U01PT0EMY2X-fc8807be507e-512'></img>
-          <h1>Matthew</h1>
+        <img src={this.state.randomStudent.imgurl}></img>
+        <h1>{this.state.randomStudent.name}</h1>
         </div>
-        <button>Randomize</button>
+        <button onClick={this.getRandomStudent}>Randomize</button>
       </div>
     )
   }
--- server/controller.js
+++ server/controller.js
@@ -5,13 +5,40 @@ const controller = {
     getStudents: function (req, res) {
       // TODO: add your code here to fetch all students
 
+      let queryStr = 'select * from students left outer join images on students.id = images.id'
+
+      db.query(queryStr, (err, result) => {
+        if (err) {
+          res.status(400).send(err)
+        } else {
+          res.status(200).send(result)
+        }
+      })
+
+
     },
     postName: function (req, res) {
       // TODO: add your code here to add a student's name
+      db.query(`insert into students (name) values ("${req.body.name}")`, (err, result) => {
+        if (err) {
+          res.status(400).send(err)
+        } else {
+          res.status(200).send(result)
+        }
+      })
 
     },
     updateName: function (req, res) {
       // TODO: add your code here to update a student's name
+      let { name } = req.body
+
+      db.query(`update students set name = "${name}" where id = ${req.params.id}`, (err, result) => {
+        if (err) {
+          res.status(400).send(err)
+        } else {
+          res.status(200).send(result)
+        }
+      })
 
     }
   },
@@ -19,6 +46,15 @@ const controller = {
   images: {
     postImg: function (req, res) {
       // TODO: add your code here to add a student image
+      let { imgurl } = req.body
+
+      db.query(`insert into images (imgurl) values ("${imgurl}")`, (err, result) => {
+        if (err) {
+          res.status(400).send(err)
+        } else {
+          res.status(200).send(result)
+        }
+      })
 
     }
   }
